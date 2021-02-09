aed73d402f2953eda21d75c4e7d689610d018ce9
WilliamChavula <rumbani@gmail.com>
2021-02-09 14:16:43 -0600

    implemented server-side rendering with StaticRouter and ReactDomServer. server rendering not 100%

--- server/express.js
+++ server/express.js
@@ -1,5 +1,14 @@
 import path from 'path';
 import express from 'express';
+
+//modules for server side rendering
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
+import StaticRouter from 'react-router-dom/StaticRouter';
+import MainRouter from '../client/MainRouter';
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
+import theme from '../client/theme';
+
 // import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
@@ -28,9 +37,33 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 app.use('/', authRoutes);
 app.use('/', userRoutes);
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template());
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets();
+    const context = {};
+    const markup = ReactDomServer.renderToString(
+        sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+                <ThemeProvider theme={theme}>
+                    <MainRouter />
+                </ThemeProvider>
+            </StaticRouter>
+        )
+    );
+    if (context.url) {
+        return res.redirect(303, context.url);
+    }
+    const css = sheets.toString();
+    res.status(200).send(
+        Template({
+            markup: markup,
+            css: css,
+        })
+    );
 });
+
+// app.get('/', (req, res) => {
+//     res.status(200).send(Template());
+// });
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
