45a3fcf84b3ab130bc1058245fb49859aadbea03
WilliamChavula <rumbani@gmail.com>
2021-02-08 21:09:25 -0600

    implemented: Private route Component, Functions to connect and fetch data from the express-server and auth API end points. implemented jwt storage in sessionStorage.

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,32 @@
+const jwt = require('express-jwt');
+
+function authenticate(hwt, cb) {
+    if (typeof window !== 'undefined') {
+        sessionStorage.setItem('jwt', JSON.stringify(jwt));
+    }
+    cb();
+}
+
+function isAuthenticated() {
+    if (typeof window === 'undefined') {
+        return false;
+    }
+
+    if (sessionStorage.getItem('jwt')) {
+        return JSON.parse(sessionStorage.getItem('jwt'));
+    } else {
+        return false;
+    }
+}
+
+function clearJWT(cb) {
+    if (typeof window !== 'undefined') {
+        sessionStorage.removeItem('jwt');
+    }
+    cb();
+    signout().then(data => {
+        document.cookie = 't=; expires=Thur, 01 Jan 1970 00:00:00 UTC; path=/;';
+    });
+}
+
+export default { authenticate, isAuthenticated, clearJWT };
